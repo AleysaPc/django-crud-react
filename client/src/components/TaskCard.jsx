0ecb567d5dda6c8a3ef577f040d06f4be5d4ef5b
Ale <cruzmaralis17@gmail.com>
2025-01-23 18:28:09 -0400

    Actualizacion

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 export function TaskCard({ task }) {
     const navigate = useNavigate()
     return (
-        <div className='bg-zinc-800 p-3 hover:bg-zinc-700 hover:cursor-pointer'
+        <div className='bg-zinc-800 p-3 hover:bg-orange-400 hover:cursor-pointer'
             onClick={() => {
                 navigate(`/tasks/${task.id}`) //redirige 
             }}
